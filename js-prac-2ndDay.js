6485c4eb56fc9d407180bc56652054677bd33c31
MATHEMATICS TERRAINS <amankumarpandey2397@gmail.com>
2022-06-29 23:29:08 +0530

    using slice methode

--- js-prac-2ndDay.js
+++ js-prac-2ndDay.js
@@ -1,3 +1,11 @@
 // length property returns the length of a string
 let text= 'anoskodjlcdmlkcndkckldmcljcsdklnslkdlwks'
 console.log(text.length);
+
+// extracting string using #slice #subtracting #substr
+let string = 'Apple, Banana, Kiwi';
+console.log(string.slice(7,13));
+console.log(string.slice(15,19));
+console.log(string.slice(0,5));
+console.log(string.slice(-12,-6));
+console.log(string.slice(7));
