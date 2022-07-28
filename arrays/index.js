301945afae2013473ec999cfe2836c7feabe7486
knightch <amankumarpandey2397@gmail.com>
2022-07-28 23:17:48 +0530

    starting arrays

---
+++ arrays/index.js
@@ -0,0 +1,19 @@
+var avgTemp=[]
+avgTemp[0]=17
+avgTemp[1]=23
+avgTemp[2]=30
+avgTemp[3]=31
+avgTemp[4]=34
+avgTemp[5]=30
+avgTemp[6]=26
+avgTemp[7]=25
+avgTemp[8]=26
+avgTemp[9]=23
+avgTemp[10]=20
+avgTemp[11]=18
+
+// initialize arrays in different methods
+var daysOfWeek1stWay = new Array() // first way
+var daysOfWeek2ndWay = new Array(7) // second way 
+var daysOfWeek3rdWay = new Array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday')
+
